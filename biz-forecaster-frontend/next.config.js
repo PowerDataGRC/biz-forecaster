360cc90a8570812a01c266d38f86090f932ae154
Murray S. <shabestari@gmail.com>
2025-11-11 20:40:47 -0600

    FIX: compile error webpack

--- biz-forecaster-frontend/next.config.js
+++ biz-forecaster-frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     reactStrictMode: true,
     // This rewrites rule acts as a proxy during development.
     // It forwards any request made to the frontend's /api/* path
@@ -13,6 +14,14 @@ const nextConfig = {
                 destination: `${process.env.BACKEND_URL}/:path*`,
             },
         ];
+    },
+    webpack: (config) => {
+        // This is the crucial part. It tells webpack to use the tsconfig.json
+        // for resolving path aliases.
+        config.resolve.plugins = [
+            ...(config.resolve.plugins || []),
+        ];
+        return config;
     },
 };
 
